1249305107952d5b47a681c3329db76cad8a4d4e
shazzatherazza <52185121+shazzatherazza@users.noreply.github.com>
2020-11-18 21:45:53 +1100

    Added profile images

--- src/components/CharacterProfile.js
+++ src/components/CharacterProfile.js
@@ -1,50 +1,68 @@
 import React, { useState, useEffect } from "react";
 import NotFound from "./NotFound";
 import axios from "axios";
-const CharacterProfile = props => {
-  const [name, setName] = useState();
-  const [homeworld, setHomeworld] = useState();
-  const [films, setFilms] = useState();
-  const [isLoading, setIsLoading] = useState(true);
-  const query = props.match.params.id;
+const CharacterProfile = (props) => {
+	const [name, setName] = useState();
+	const [homeworld, setHomeworld] = useState();
+	const [films, setFilms] = useState();
+	const [image, setImage] = useState();
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState();
+	const query = props.match.params.id;
+	let bingSubscriptionKey = "bb463914a7bc4a6f8b3fe165c202f560";
 
-  useEffect(() => {
-    axios(`https://swapi.dev/api/people/` + query)
-      .then(response => {
-        setName(response.data.name);
-        let promises = [];
-        promises.push(axios(response.data.homeworld));
+	useEffect(() => {
+		axios(`https://swapi.dev/api/people/` + query)
+			.then((response) => {
+				setName(response.data.name);
+				let promises = [];
+				promises.push(
+					axios.get("https://api.bing.microsoft.com/v7.0/images/search", {
+						headers: {
+							"Ocp-Apim-Subscription-Key": bingSubscriptionKey,
+						},
+						params: {
+							count: 1,
+							mkt: "en-US",
+							q: response.data.name,
+						},
+					})
+				);
 
-        response.data.films.forEach(film => promises.push(axios(film)));
-        return Promise.all(promises);
-      })
-      .then(response => {
-        setHomeworld(response[0].data.name);
-        setFilms(response.slice(1).map(film => film.data.title));
-      })
-      .catch(error => console.log("Error fetching and parsing data", error))
-      .finally(() => setIsLoading(false));
-  }, [query]);
+				promises.push(axios(response.data.homeworld));
 
-  let comp;
-  if (isLoading) {
-    comp = <p>Loading...</p>;
-  } else if (name && homeworld && films) {
-    comp = (
-      <div>
-        <h1>{name}</h1>
-        <h2>Homeworld: {homeworld}</h2>
-        <ul>
-          {films.map(film => (
-            <li key={film}>{film}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  } else {
-    comp = <NotFound />;
-  }
-  return <div className="main-content">{comp}</div>;
+				response.data.films.forEach((film) => promises.push(axios(film)));
+				return Promise.all(promises);
+			})
+			.then((response) => {
+				setImage(response[0].data.value[0].thumbnailUrl);
+				setHomeworld(response[1].data.name);
+				setFilms(response.slice(2).map((film) => film.data.title));
+			})
+			.catch((error) => setError(error))
+			.finally(() => setIsLoading(false));
+	}, [query]);
+
+	let comp;
+	if (isLoading) {
+		comp = <p>Loading...</p>;
+	} else if (!error) {
+		comp = (
+			<div>
+				<h1>{name}</h1>
+				<h2>Homeworld: {homeworld}</h2>
+				<ul>
+					{films.map((film) => (
+						<li key={film}>{film}</li>
+					))}
+				</ul>
+				<img src={image} alt={name} />
+			</div>
+		);
+	} else {
+		comp = <NotFound />;
+	}
+	return <div className="main-content">{comp}</div>;
 };
 
 export default CharacterProfile;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,26 +2,26 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 function Home() {
-  let history = useHistory();
-  const [name, setName] = useState("");
-  const handleSubmit = e => {
-    e.preventDefault();
-    let searchName = name.value;
-    let path = `search/${searchName}`;
-    history.push(path);
-  };
+	let history = useHistory();
+	const [name, setName] = useState("");
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let searchName = name.value;
+		let path = `search/${searchName}`;
+		history.push(path);
+	};
 
-  return (
-    <div className="main-content home">
-      <h1>Perx Star Wars Frontend Coding Challenge</h1>
-      <h2>Star Wars character search</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Name" ref={input => setName(input)} />
+	return (
+		<div className="main-content">
+			<h1>Perx Star Wars Frontend Coding Challenge</h1>
+			<h2>Star Wars character search</h2>
+			<form onSubmit={handleSubmit}>
+				<input type="text" placeholder="Name" ref={(input) => setName(input)} />
 
-        <button type="submit">Go!</button>
-      </form>
-    </div>
-  );
+				<button type="submit">Go!</button>
+			</form>
+		</div>
+	);
 }
 
 export default Home;
